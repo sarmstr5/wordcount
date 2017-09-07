474f13c0456de820ef2e65491632d52958c75f8d
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-07 17:40:34 -0400

    second commit

--- WordCount.java
+++ WordCount.java
@@ -21,12 +21,18 @@ public class WordCount extends Configured implements Tool {
   public static class Reduce extends MapReduceBase implements Reduce<Text, IntWritable, Text, IntWritable>{
     public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
       int sum = 0;
+      int count =0;
       while(values.hasNext()) {
         sum += values.next().get();
+        count++
       }
       // output each token
-      output.collect(key, new IntWritable(sum));
+      output.collect(key, new IntWritable(sum)); //add new IntWritable(count)
     }
   }
 }
-
+/*
+3) Count average use of everyword per year
+need to also keep track of the lenghts
+s
+*/
