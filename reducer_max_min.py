fe8043fb71f166f70c242ac91b9643f5217577f9
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-17 14:09:51 -0400

    finished max_min mapper" git push

--- reducer_max_min.py
+++ reducer_max_min.py
@@ -1,31 +1,41 @@
 #! /usr/bin/env python
 
 import sys
+from collections import defaultdict
 
-(last_key, sum, count) = (None, 0, 0)
-total=0
-last_key = False
+all_counts = defaultdict(list)
+doc_counts = defaultdict(int)
+current_doc = None
 
+# input from STDIN
+# expecting a doc number, word from document, and count for that word
 for line in sys.stdin:
-  data = line.split("\t")
-  year = data[0]
-  word = data[1]
-  count = data[2]
-  combined_key = year + '_' + word
-  
-  # found new key, emit current count
-  if last_key and current_key != last_key:
-    print(current_key, total)
-    last_key = current_key
-    total = count
+
+    line = line.strip()
+    doc_num, word, count = line.split('\t', 2)
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    # expecting hadoop to give keys sorted by doc with words in arbituary order
+    # if new document the previous document is completed
+    # append word counts for that document to master word count dictionary 
+    # create new doc dictionary to restart word count
+    if current_doc and (current_doc != doc_num):
+        for word, count in doc_counts.iteritems():
+            all_counts[word].append(count)
+        doc_counts = defaultdict(int)
 
 
-  # same key increment count
-  else:
-    last_key = current_key
-    total += count
+    # same document continue incrementing document word counts
+    else:
+        doc_counts[word] += count
 
-# last key
-if last_key:
-  print(last_key, total)
-
+# went through all mapped items
+for word, counts in all_counts.iteritems():
+    max_wc = max(counts)
+    min_wc = min(counts)
+    print('{}\t{}\t{}'.format(word, min_wc, max_wc))
+    
--- test_script.sh
+++ test_script.sh
@@ -1,1 +1,4 @@
 cat 2017022.txt | python average_by_attribute_mapper.py | sort -k1,1 | python test_reducer.py
+for f in *.txt; do (cat "${f}"; echo) done
+awk 'FNR==1 && NR>1{print ""}1' *.txt
+
