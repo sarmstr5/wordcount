d6ddc0a3a1cd41da3eae70e262a7a9932f1d9c4f
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-18 01:00:56 -0400

    added rolling avg run

--- test_script.sh
+++ test_script.sh
@@ -2,3 +2,4 @@ cat 2017022.txt | python average_by_attribute_mapper.py | sort -k1,1 | python te
 for f in *.txt; do (cat "${f}"; echo) done
 awk 'FNR==1 && NR>1{print ""}1' *.txt
 
+for f in *.txt; do (cat "${f}"; echo) done | python2 map_4yr_avg.py | sort -k1,1 | python2 reducer_4yr_avg.py
