7c001c760da5f5ce70dfcc5bbd874b9761edcade
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-17 23:52:20 -0400

    worked with linux test

--- map_4yr_avg.py
+++ map_4yr_avg.py
@@ -5,48 +5,37 @@ import sys
 import re
 from collections import defaultdict
 
-# initializes value as zero when new key is added
-word_dict = defaultdict(list)
+# initializes values as zeros array of length 5 when new key is added
+word_dict = defaultdict(lambda : [0]*5)
 
 # looking at partitioning the addresses into 'presidential terms'
-term_window = 3
+# years start at zero due to list indexing
+# the term window is one year longer to store the camparison year
+term_window = 4
 term_year = 0
 year_partition = 0
-first_year_next_term = false
-
-#input_file = os.environ['map_input_file']
-#date = input_file.split('.')[0]
-#date = int(re.search('\d+', input_file)
-#for a in os.environ:
-#    print('Var: {}\tValue: {}'.format(a, os.getenv(a)))
 
 for doc in sys.stdin:
     data = doc.split()
     for line in data:
         words = line.split()
         for word in words:
-            if word in word_dict:
-                word_dict[word][term_year] += 1
-            else:
-                :w
-
-            # to save network traffic save in dictionary and print summed counts
-
+              word_dict[word][term_year] += 1
 
     # want the words to be sorted by partition for the reducer
     # sorted words for validation purposes but not needed
-    if year_count == term_window:
+    if term_year == term_window:
         print('in if loop')
         for word in sorted(word_dict.iterkeys()):
-            print('{}\t{}\t{}'.format(year_partition-1, first_year_next_term, word, word_dict[word]))
-        year_count = 1
+            print('{}\t{}\t{}'.format(year_partition, word, word_dict[word]))
+            if word_dict[word][term_window] == 0:
+              del word_dict[word]
+            else:
+              word_dict[word] = [word_dict[word][term_window], 0, 0, 0, 0]
+        term_year = 0
         year_partition += 1
-        word_dict = defaultdict(int)  # overwrite dictionary to save RAM
     else:
-        if first_year_next_term:
-            for word in sorted(word_dict.iterkeys()):
-                print('{}\t{}\t{}'.format(year_partition-1, first_year_next_term, word, word_dict[word]))
-
         print('year count ++')
-        year_count += 1
+        term_year += 1
+
 
