cbae695dc78ee16bc23c7f5d301a60b4d9e82ea2
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-17 23:17:01 -0400

    fix 5 year dict window

--- map_4yr_avg.py
+++ map_4yr_avg.py
@@ -6,12 +6,13 @@ import re
 from collections import defaultdict
 
 # initializes value as zero when new key is added
-word_dict = defaultdict(int)
+word_dict = defaultdict(list)
 
 # looking at partitioning the addresses into 'presidential terms'
-term_window = 4
-year_count = 1
+term_window = 3
+term_year = 0
 year_partition = 0
+first_year_next_term = false
 
 #input_file = os.environ['map_input_file']
 #date = input_file.split('.')[0]
@@ -24,18 +25,28 @@ for doc in sys.stdin:
     for line in data:
         words = line.split()
         for word in words:
+            if word in word_dict:
+                word_dict[word][term_year] += 1
+            else:
+                :w
+
             # to save network traffic save in dictionary and print summed counts
-            word_dict[word] += 1
 
 
     # want the words to be sorted by partition for the reducer
     # sorted words for validation purposes but not needed
     if year_count == term_window:
+        print('in if loop')
         for word in sorted(word_dict.iterkeys()):
-            print('{}\t{}\t{}'.format(year_partition, word, word_dict[word]))
+            print('{}\t{}\t{}'.format(year_partition-1, first_year_next_term, word, word_dict[word]))
         year_count = 1
         year_partition += 1
         word_dict = defaultdict(int)  # overwrite dictionary to save RAM
     else:
+        if first_year_next_term:
+            for word in sorted(word_dict.iterkeys()):
+                print('{}\t{}\t{}'.format(year_partition-1, first_year_next_term, word, word_dict[word]))
+
+        print('year count ++')
         year_count += 1
 
