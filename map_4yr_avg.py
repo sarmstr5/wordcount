4e85a1e9f0dd29528c3c99ef159c9f7566789848
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-21 15:56:15 -0400

    update

--- map_4yr_avg.py
+++ map_4yr_avg.py
@@ -15,11 +15,12 @@ word_dict = defaultdict(int)
 # the term window is one year longer to store the camparison year
 term_window = 3
 term_year = 0
-year_partition = 1985
+partition = 0
 year = 1985
-doc_count = 0
 
 for doc in sys.stdin:
+    print(doc)
+
     try:
         # files are YYYYMMDD.txt
         input_file = os.environ['mapreduce_map_input_file']
@@ -29,31 +30,22 @@ for doc in sys.stdin:
             input_file = os.environ['map_input_file']
             date = input_file.split('.')[0][:4]
         except KeyError:
-            date = doc_count
+            date = year
 
     words = doc.split()
     for word in words:
-      word_dict[word] += 1
-    for word, count in sorted(word_dict.iterkeys()):
-      print('{}\t{}\t{}'.form(date, word, count)
-    doc_count += 1
+        word_dict[word] += 1
 
-    # should this if be first?
-    if term_year == term_window:
-        print('in new term year {}'.format(year))
-        for word in sorted(word_dict.iterkeys()):
-#           print('{}\t{}\t{}'.format(year_partition, word, word_dict[word]))
-            if word_dict[word][term_window] == 0:
-              del word_dict[word]
-            else:
-              # shift count from index 4 to 0
-              word_dict[word] = [word_dict[word][term_window], 0, 0, 0, 0]
+    for word in sorted(word_dict.iterkeys()):
+        print('{}\t{}\t{}\t{}'.format(partition, date, word, word_dict[word]))
+
+    if(term_year == term_window):
+        partition += 1
         term_year = 0
-        year_partition += 4
-        year+=1
     else:
-      print('same term year {}'.format(year))
-    term_year += 1
+        term_year += 1
     year += 1
+
+
 
 
--- reducer_4yr_avg.py
+++ reducer_4yr_avg.py
@@ -5,69 +5,33 @@ from collections import defaultdict
 import ast
 import numpy as np
 
-# initializes values as zeros array of length i when new key is added
-# made it 6 incase of multiple sotu addresses in one year
-# word_dict = defaultdict(lambda : [0]*6)
-word_dict = defaultdict(int)
-
-# looking at partitioning the addresses into 'presidential terms'
-# years start at zero due to list indexing
-# the term window is one year longer to store the camparison year
-term_window = 4
-term_year = 0
-year_partition = 1985
-year = 1985
 
 # because partition is an int starting at 0 i could do a list of tuples for
 # all_counts
 all_counts = defaultdict(list)
-doc_counts = defaultdict(lambda : [0]*5)
+doc_counts = defaultdict(lambda : [0]*6)
 sig_words = defaultdict(list)
 cur_partition = None
 first_year = 1989
 
 def calc_avg_std(word_counts):
-  previous_counts = word_counts[:-1]
-  x = word_counts[-1]
-  avg = np.mean(previous_counts)
-  std = np.std(previous_counts)
-# if x == 0 or avg == 0:
-#   return 0
-  z_score = (x - avg)/(std)
-  return abs(round(z_score,2))
+  avg = np.mean(word_counts)
+  return abs(round(avg))
 
 
 # input from STDIN
-# expecting a year, word, list of 5 word counts from each year 
-# 1985 \t young \t 6
+# expecting partition \ year \ word \ year
+# 0 \t 1985 \t young \t 6
 for line in sys.stdin:
-  term_partition, word, counts = line.split('\t')
+  cur_partition, year, word, count = line.split('\t')
   try:
-      counts = ast.literal_eval(counts)
-      # do i need to error test this?
-      counts = [int(i) for i in counts]
+      count = int(count)
       term_partition = int(term_partition)
   except ValueError:
       continue
-
     # want the words to be sorted by partition for the reducer
     # sorted words for validation purposes but not needed
     # should this if be first?
-    if term_year == term_window:
-        print('in new term year {}'.format(year))
-        for word in sorted(word_dict.iterkeys()):
-#           print('{}\t{}\t{}'.format(year_partition, word, word_dict[word]))
-            if word_dict[word][term_window] == 0:
-              del word_dict[word]
-            else:
-              # shift count from index 4 to 0
-              word_dict[word] = [word_dict[word][term_window], 0, 0, 0, 0]
-        term_year = 0
-        year_partition += 4
-        year+=1
-        
-    else:
-        print('same term year {}'.format(year))
 
 
   new_term = first_year + 4 * term_partition
@@ -75,7 +39,7 @@ for line in sys.stdin:
   # if new document, the previous document is completed
   # calc average
   # create new doc dictionary to restart word count
-  if cur_partition and (cur_partition != term_partition):
+  if cur_partition and (cur_partition != last_partition):
     for word, counts in doc_counts.iteritems():
       z_score = calc_avg_std(counts)
       if z_score > 2:
@@ -89,8 +53,3 @@ for line in sys.stdin:
     for i in range(len(counts)):
       doc_counts[word][i] += counts[i]
 
-# went through all mapped items
-for term, term_list in all_counts.iteritems():
-  for tup in term_list:
-    (word, z_score) = tup
-    print('{}\t{}\t{}'.format(term, word, z_score))
