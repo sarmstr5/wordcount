8452c46c71db44ca4ac0c218377b75cdb7c2d0e7
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-21 15:26:12 -0400

    updates

--- reducer_4yr_avg.py
+++ reducer_4yr_avg.py
@@ -5,6 +5,19 @@ from collections import defaultdict
 import ast
 import numpy as np
 
+# initializes values as zeros array of length i when new key is added
+# made it 6 incase of multiple sotu addresses in one year
+# word_dict = defaultdict(lambda : [0]*6)
+word_dict = defaultdict(int)
+
+# looking at partitioning the addresses into 'presidential terms'
+# years start at zero due to list indexing
+# the term window is one year longer to store the camparison year
+term_window = 4
+term_year = 0
+year_partition = 1985
+year = 1985
+
 # because partition is an int starting at 0 i could do a list of tuples for
 # all_counts
 all_counts = defaultdict(list)
@@ -36,6 +49,26 @@ for line in sys.stdin:
       term_partition = int(term_partition)
   except ValueError:
       continue
+
+    # want the words to be sorted by partition for the reducer
+    # sorted words for validation purposes but not needed
+    # should this if be first?
+    if term_year == term_window:
+        print('in new term year {}'.format(year))
+        for word in sorted(word_dict.iterkeys()):
+#           print('{}\t{}\t{}'.format(year_partition, word, word_dict[word]))
+            if word_dict[word][term_window] == 0:
+              del word_dict[word]
+            else:
+              # shift count from index 4 to 0
+              word_dict[word] = [word_dict[word][term_window], 0, 0, 0, 0]
+        term_year = 0
+        year_partition += 4
+        year+=1
+        
+    else:
+        print('same term year {}'.format(year))
+
 
   new_term = first_year + 4 * term_partition
   print(new_term)
