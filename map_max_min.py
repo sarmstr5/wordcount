672268bf1f6d83562031dbcfe9741056c76be7d2
sarmstr5 <git.sarmstr5@gmail.com>
2017-09-21 13:15:41 -0400

    minor updates

--- map_max_min.py
+++ map_max_min.py
@@ -6,26 +6,29 @@ import re
 from collections import defaultdict
 
 # initializes value as zero when new key is added
+
 word_dict = defaultdict(int)
 doc_count = 0
 
-#input_file = os.environ['map_input_file']
-#date = input_file.split('.')[0]
-#date = int(re.search('\d+', input_file)
-#for a in os.environ:
-#    print('Var: {}\tValue: {}'.format(a, os.getenv(a)))
-
 for doc in sys.stdin:
-    data = doc.split()
-    for line in data:
-        words = line.split()
-        for word in words:
-            # to save network traffic
-            word_dict[word] += 1
+    try:
+        input_file = os.environ['mapreduce_map_input_file']
+        date = input_file.split('.')[0][:4]
+    except KeyError:
+        try:
+            input_file = os.environ['map_input_file']
+            date = input_file.split('.')[0][:4]
+        except KeyError:
+            date = doc_count
+
+    words = doc.split()
+    for word in words:
+        # to save network traffic
+        word_dict[word] += 1
 
     # dont need to sort words but helps with validation
     for word in sorted(word_dict.iterkeys()):
-        print('{}\t{}\t{}'.format(doc_count, word, word_dict[word]))
+        print('{}\t{}\t{}'.format(date, word, word_dict[word]))
 
     word_dict = defaultdict(int)
     doc_count += 1
--- reducer_max_min.py
+++ reducer_max_min.py
@@ -6,13 +6,13 @@ from collections import defaultdict
 all_counts = defaultdict(list)
 doc_counts = defaultdict(int)
 current_doc = None
+last_doc = None
 
 # input from STDIN
 # expecting a doc number, word from document, and count for that word
 for line in sys.stdin:
-
-    doc_num, word, count = line.split('\t')
-
+    current_doc, word, count = line.split('\t')
+    print( 'new doc: {} word:{} count:{}'.format(current_doc, word, count)) 
     try:
         count = int(count)
     except ValueError:
@@ -22,7 +22,8 @@ for line in sys.stdin:
     # if new document the previous document is completed
     # append word counts for that document to master word count dictionary 
     # create new doc dictionary to restart word count
-    if current_doc and (current_doc != doc_num):
+    if current_doc and (current_doc != last_doc):
+        print('in new doc')
         for word, count in doc_counts.iteritems():
             all_counts[word].append(count)
         doc_counts = defaultdict(int)
@@ -30,11 +31,15 @@ for line in sys.stdin:
 
     # same document continue incrementing document word counts
     else:
-        current_doc = doc_num
+        print('same doc')
+        last_doc = current_doc
         doc_counts[word] += count
 
 # went through all mapped items
+print('went though mapped items')
 for word, counts in all_counts.iteritems():
+    print(word)
+    print(counts)
     max_wc = max(counts)
     min_wc = min(counts)
     print('{}\t{}\t{}'.format(word, min_wc, max_wc))
